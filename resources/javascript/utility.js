cdda5c90baaad8072edb74997390c713d24c4a78
Melani Alderte <91097884+melaniAlderete@users.noreply.github.com>
2023-07-14 01:14:21 -0300

    Add files via upload

---
+++ resources/javascript/utility.js
@@ -0,0 +1,13 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+/*let signButton = document.getElementById("signButton");
+let signForm = document.getElementById("signForm");
+let logButton = document.getElementById("logButton");
+let logForm = document.getElementById("logForm");
+
+function animate(){
+    ({height: "toggle", opacity: "toggle"}, "slow");
+}
+signButton.onclick = animate;
+signForm.onclick = animate;*/
